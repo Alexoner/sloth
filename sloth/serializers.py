d0a70b4980b39970330f40b89a7245f7d03ea8d6
Alexoner <onerhao@gmail.com>
2016-06-03 19:36:07 +0800

    trivial

--- sloth/serializers.py
+++ sloth/serializers.py
@@ -5,7 +5,6 @@ from job.models import Proxy
 # Serializers define the API representation.
 class UserSerializer(serializers.ModelSerializer):
     proxies = serializers.PrimaryKeyRelatedField(many=True, queryset=Proxy.objects.all())
-    print('holy shit', proxies)
 
     class Meta:
         model = User
